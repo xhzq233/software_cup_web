91febe0b7818f4120cec6f42f2e971ea4f93bf00
xhzq233 <1761373255@qq.com>
2023-08-18 10:59:52 +0800

    width

--- lib/pages/home/doc_page.dart
+++ lib/pages/home/doc_page.dart
@@ -9,12 +9,12 @@ class DocPage extends StatelessWidget {
     final width = MediaQuery.sizeOf(context).width * 0.8;
     return ListView(
       children: [
-        Image.asset(Assets.manual1, height: MediaQuery.of(context).size.height, width: width),
-        Image.asset(Assets.manual2, height: MediaQuery.of(context).size.height, width: width),
-        Image.asset(Assets.manual3, height: MediaQuery.of(context).size.height, width: width),
-        Image.asset(Assets.manual4, height: MediaQuery.of(context).size.height, width: width),
-        Image.asset(Assets.manual5, height: MediaQuery.of(context).size.height, width: width),
-        Image.asset(Assets.manual6, height: MediaQuery.of(context).size.height, width: width),
+        Image.asset(Assets.manual1, fit: BoxFit.fitWidth, width: width),
+        Image.asset(Assets.manual2, fit: BoxFit.fitWidth, width: width),
+        Image.asset(Assets.manual3, fit: BoxFit.fitWidth, width: width),
+        Image.asset(Assets.manual4, fit: BoxFit.fitWidth, width: width),
+        Image.asset(Assets.manual5, fit: BoxFit.fitWidth, width: width),
+        Image.asset(Assets.manual6, fit: BoxFit.fitWidth, width: width),
       ],
     );
   }
